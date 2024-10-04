069a1d5bfab743263d3803ed177a62d2e5be8de5
Aleksei Khasanov <akhasano@gmail.com>
2024-10-04 22:47:31 +0300

    added default relative numbers

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,5 +14,8 @@ vim.cmd('set mouse=n')
 vim.cmd('set foldmethod=indent')
 vim.cmd('set foldlevel=100')
 vim.cmd('set clipboard=unnamedplus')
+vim.cmd('set number')
+vim.cmd('set relativenumber')  -- Включаем относительные номера строк
 
 return M
+
