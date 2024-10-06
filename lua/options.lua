f36b29a342964da0fccc1cbf7b26f3642f31a5e9
Aleksei Khasanov <akhasano@gmail.com>
2024-10-06 09:39:59 +0300

    Added tf/hcl comments

--- lua/options.lua
+++ lua/options.lua
@@ -20,3 +20,11 @@ require("nvim-tree").setup({
   },
 })
 
+--fix terraform and hcl comment string
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
