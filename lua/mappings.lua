3e760132b94507ad75e0c5835cb0fc064295e83b
Aleksei Khasanov <akhasano@gmail.com>
2024-10-13 11:10:46 +0300

    Hotkeys for buffers close

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,6 +13,9 @@ map("n","<leader><leader>b","<cmd> HopWord <CR>", { desc = "hint all words"} )
 map("n","<leader><leader>j","<cmd> HopLine <CR>", { desc = "hint line"} )
 map("n","<leader><leader>k","<cmd> HopLine <CR>", { desc = "hint line"} )
 
+map("n","<leader>lc","<cmd> %bd <CR>", { desc = "close all buffers"} )
+map("n","<leader>lo","<cmd> %bd|e#|bd# <CR>", { desc = "close all buffers except this"} )
+
 map("t","<Leader><ESC>","<C-\\><C-n>",{noremap = true})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
