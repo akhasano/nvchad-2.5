cb5f496cd854492d791a23999ffc5815dafc33c1
Aleksei Khasanov <akhasano@gmail.com>
2024-10-13 10:32:03 +0300

    Added <leader><esc> in terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,4 +13,6 @@ map("n","<leader><leader>b","<cmd> HopWord <CR>", { desc = "hint all words"} )
 map("n","<leader><leader>j","<cmd> HopLine <CR>", { desc = "hint line"} )
 map("n","<leader><leader>k","<cmd> HopLine <CR>", { desc = "hint line"} )
 
+map("t","<Leader><ESC>","<C-\\><C-n>",{noremap = true})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
