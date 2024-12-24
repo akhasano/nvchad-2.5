9bcc3c4409f6bd5a072e9a929c129e5dea8d7fcb
Aleksei Khasanov <akhasano@gmail.com>
2024-12-24 20:21:03 +0300

    Resized floated terminal

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,11 +1,29 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "chadracula",
+  theme = "chadracula",
 }
+
+M.ui = {
+  telescope = {
+    style = "bordered",
+  },
+}
+
+M.term = {
+  float = {
+    border = "rounded", -- стиль границы окна (можно использовать "single", "double" и др.)
+    row = 0.05,
+    col = 0.05,
+    width = 0.9,
+    height = 0.8,
+  }
+}
+
+
 return M
