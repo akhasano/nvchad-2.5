14f0d0d70d356925c4af560d2bbf30ba1bdff732
Aleksei Khasanov <akhasano@gmail.com>
2025-01-02 22:41:38 +0300

    Fixed clipboard setup

--- lua/options.lua
+++ lua/options.lua
@@ -9,7 +9,7 @@ vim.cmd('set autochdir')
 vim.cmd('set mouse=n')
 vim.cmd('set foldmethod=indent')
 vim.cmd('set foldlevel=100')
-vim.cmd('set clipboard=unnamedplus')
+vim.cmd('set clipboard+=unnamedplus')
 vim.cmd('set number')
 vim.cmd('set relativenumber')  -- Включаем относительные номера строк
 vim.cmd('set whichwrap=b,s') -- Выключить переход на новую строку при достижении конца строки курсором
