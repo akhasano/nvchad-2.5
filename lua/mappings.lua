87fc6d13c19931c4d0bc2dda9ca33a6a5ffccf03
Aleksei Khasanov <akhasano@gmail.com>
2025-01-03 11:04:19 +0300

    Added quickfix hotkey

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,14 +8,26 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
 
-map("n","<leader><leader>w","<cmd> HopWord <CR>", { desc = "hint all words"} )
-map("n","<leader><leader>b","<cmd> HopWord <CR>", { desc = "hint all words"} )
-map("n","<leader><leader>j","<cmd> HopLine <CR>", { desc = "hint line"} )
-map("n","<leader><leader>k","<cmd> HopLine <CR>", { desc = "hint line"} )
+map("n", "<leader><leader>w", "<cmd> HopWord <CR>", { desc = "hint all words" })
+map("n", "<leader><leader>b", "<cmd> HopWord <CR>", { desc = "hint all words" })
+map("n", "<leader><leader>j", "<cmd> HopLine <CR>", { desc = "hint line" })
+map("n", "<leader><leader>k", "<cmd> HopLine <CR>", { desc = "hint line" })
 
-map("n","<leader>lc","<cmd> %bd <CR>", { desc = "close all buffers"} )
-map("n","<leader>lo","<cmd> %bd|e#|bd# <CR>", { desc = "close all buffers except this"} )
+map("n", "<leader>lc", "<cmd> %bd <CR>", { desc = "close all buffers" })
+map("n", "<leader>lo", "<cmd> %bd|e#|bd# <CR>", { desc = "close all buffers except this" })
 
-map("t","<Leader><ESC>","<C-\\><C-n>",{noremap = true})
+map("t", "<leader><ESC>", "<C-\\><C-n>", { noremap = true })
+
+-- quickfix for LSP
+local opts = { noremap = true, silent = true }
+
+local function quickfix()
+  vim.lsp.buf.code_action({
+    filter = function(a) return a.isPreferred end,
+    apply = true
+  })
+end
+
+map("n", "<leader>qf", quickfix, opts)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
