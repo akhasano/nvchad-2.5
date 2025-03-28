a5c62af3635679fa0b9f12f4df63e5e0ff4bde94
Aleksey Khasanov <aleksey.khasanov@x5.ru>
2025-03-28 10:15:42 +0300

    Disabled relative numbers

--- lua/options.lua
+++ lua/options.lua
@@ -11,8 +11,8 @@ vim.cmd('set foldmethod=indent')
 vim.cmd('set foldlevel=100')
 vim.cmd('set clipboard+=unnamedplus')
 vim.cmd('set number')
-vim.cmd('set relativenumber') -- Включаем относительные номера строк
-vim.cmd('set whichwrap=b,s')  -- Выключить переход на новую строку при достижении конца строки курсором
+-- vim.cmd('set relativenumber') -- Включаем относительные номера строк
+vim.cmd('set whichwrap=b,s') -- Выключить переход на новую строку при достижении конца строки курсором
 vim.cmd('set wildmenu')
 vim.cmd('set wildmode=longest:full,full')
 vim.cmd('set wildoptions=pum')
