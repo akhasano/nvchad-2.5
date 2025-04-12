8a876d68f7145b605d5666ffcc17dfc3c3c63cc3
Aleksei Khasanov <akhasano@gmail.com>
2025-04-13 00:38:47 +0300

    Added debug for golang + console

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -5,7 +5,7 @@ dap.adapters.go = {
   port = 62345,
   executable = {
     command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
-    args = { "dap", "-l", "127.0.0.1:62345" },
+    args = { "dap", "--log", "-l", "127.0.0.1:62345" },
     detached = true,
   }
 }
@@ -13,9 +13,52 @@ dap.adapters.go = {
 dap.configurations.go = {
   {
     type = "go",
-    name = "Debug",
+    name = "Debug (args + .env.debug)",
     request = "launch",
-    program = "${file}"
+    program = "${file}",
+    -- console = 'integratedTerminal',
+    -- outputCapture = "std", -- Перехватывать stdout/stderr
+    outputMode = "remote",
+    showLog = true,
+    showRegisters = false,
+    stopOnEntry = false,
+
+    args = function()
+      local input = vim.fn.input("Args: ")
+      if input == nil or input == "" then
+        return {}
+      end
+      return vim.fn.split(input, " ", true)
+    end,
+
+    env = function()
+      local env_file = vim.fn.getcwd() .. "/.env.debug"
+
+      if vim.fn.filereadable(env_file) == 0 then
+        print("⚠️ .env.debug not found")
+        return nil
+      end
+
+      local envs = {}
+      for line in io.lines(env_file) do
+        if line:match("^%s*$") == nil and not line:match("^%s*#") then
+          local key, val = line:match("^%s*([%w_]+)%s*=%s*(.*)%s*$")
+          if key and val then
+            envs[key] = val
+          else
+            print("⚠️ Невозможно распарсить строку: " .. line)
+          end
+        end
+      end
+
+      if next(envs) == nil then
+        print("⚠️ .env.debug найден, но пуст или нераспознан")
+        return nil
+      end
+
+      print("✅ .env.debug загружен")
+      return envs
+    end,
   },
 }
 
