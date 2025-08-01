f11065d534e6d7ce1deeabe2487754309662500f
Aleksey Khasanov <aleksey.khasanov@x5.ru>
2025-08-01 20:56:49 +0300

    Added context on top

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -53,6 +53,25 @@ return {
   },
 
   {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesitter-context").setup {
+        enable = true,            -- Включить контекст
+        max_lines = 5,            -- Максимальное количество отображаемых строк контекста (0 для неограниченного)
+        min_window_height = 0,    -- Минимальная высота окна для отображения контекста
+        line_numbers = true,      -- Отображать номера строк
+        multiline_threshold = 20, -- Количество строк, после которого отображается многострочный контекст
+        trim_scope = "outer",     -- Обрезка контекста: 'inner' (только текущий scope), 'outer' (все вложенные)
+        mode = "cursor",
+        separator = "-",
+        on_attach = nil,
+      }
+    end,
+  },
+
+  {
     -- Hop is an EasyMotion-like plugin allowing you to jump anywhere in a document with as few keystrokes as possible.
     "smoka7/hop.nvim",
     -- lazy = false,
