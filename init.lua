fda75b5fefa12997555c9e4f5ec7f7a9dacb2908
Aleksey Khasanov <aleksey.khasanov@x5.ru>
2025-08-29 08:10:12 +0300

    Neovide as notepad

--- init.lua
+++ init.lua
@@ -25,6 +25,28 @@ require("lazy").setup({
   { import = "plugins" },
 }, lazy_config)
 
+if vim.g.neovide then
+  local autosave_dir = vim.fn.expand("~/tmp/.neovide-autosave")
+  vim.fn.mkdir(autosave_dir, "p")
+  vim.cmd("cd " .. autosave_dir)
+
+  vim.api.nvim_create_augroup("NeovideAutoSaveGroup", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "BufNewFile", "BufEnter" }, {
+    group = "NeovideAutoSaveGroup",
+    callback = function()
+      local bufname = vim.api.nvim_buf_get_name(0)
+      if bufname == "" then
+        local filename = os.date("%Y%m%d-%H%M%S")
+        local filepath = autosave_dir .. "/" .. filename
+        vim.api.nvim_buf_set_name(0, filepath)
+        vim.cmd("write")
+      end
+    end,
+  })
+end
+
+
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
